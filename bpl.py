4e50557199abb068966a7e4b525f7b772f20c294
Philip Chou <pcchou@pcchou.me>
2015-05-16 21:04:58 +0800

    fixed issues and coding style

--- bpl.py
+++ bpl.py
@@ -11,143 +11,145 @@ import bplcookies # Please input your own BAHAID and BAHARUNE cookies.
 REQHEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"}
 REQCOOKIES = {"BAHAID": bplcookies.BAHAID, "BAHARUNE": bplcookies.BAHARUNE}
 
-class bahaPost:
+
+def from_url(inurl):
+    """ Initiates a bpl object (BahaPost or Floor) with an URL """
+    parsed = urlparse.urlparse(inurl)
+    params = dict(urlparse.parse_qsl(parsed.query))
+
+    if (parsed.scheme == "http" and
+            parsed.netloc == "forum.gamer.com.tw" and
+            parsed.path == "/C.php" and
+            all(x in params for x in ["bsn", "sna"])):
+        return BahaPost(params["bsn"], params["sna"])
+    elif (parsed.scheme == "http" and
+          parsed.netloc == "forum.gamer.com.tw" and
+          parsed.path == "/Co.php" and
+          all(x in params for x in ["bsn", "sn"])):
+        return Floor(params["bsn"], params["sn"])
+    else:
+        raise ValueError("Input is not a vaild bahaurl.")
+
+class BahaPost:
     """ An object class used to describe a baha post. """
-    def __init__(self, bsn, snA):
-        """ Initiates a bahaPost object """
+    def __init__(self, bsn, sna):
+        """ Initiates a BahaPost object """
         self.bsn = bsn
-        self.snA = snA
-        self.url = "http://forum.gamer.com.tw/C.php?" + "bsn=" + str(self.bsn) + "&" + "snA=" + str(self.snA)
-
-    def from_url(inurl):
-        """ Initiates a bahapost object with an URL """
-        parsed = urlparse.urlparse(inurl)
-        params = dict(urlparse.parse_qsl(parsed.query))
-
-        if (parsed.scheme == "http" and
-                parsed.netloc == "forum.gamer.com.tw" and
-                parsed.path == "/C.php" and
-                all(x in params for x in ["bsn", "snA"])):
-            return bahaPost(params["bsn"], params["snA"])
-        else:
-            raise ValueError("Input is not a vaild bahaurl.")
+        self.sna = sna
+        self.url = ("http://forum.gamer.com.tw/C.php?" +
+                    "bsn=" + str(self.bsn) + "&" + "sna=" + str(self.sna))
 
     @property
-    def floors_snB(self):
-        """ snB list of the bahapost """
+    def floors_snb(self):
+        """ snb list of the BahaPost """
         ret = []
 
         soup = BeautifulSoup(
-            requests.get( "http://forum.gamer.com.tw/C.php",
-                    params = {"bsn": self.bsn, "snA": self.snA},
-                    headers=REQHEADERS,
-                    cookies=REQCOOKIES).text
+            requests.get("http://forum.gamer.com.tw/C.php",
+                         params={"bsn": self.bsn, "sna": self.sna},
+                         headers=REQHEADERS,
+                         cookies=REQCOOKIES).text
         )
 
         for gpword in soup.body("a", {"class": "GPword"}):
             ret.append(
-                    re.search("upgp_(\d+)", gpword.attrs["id"]).group(1))
+                re.search(r'upgp_(\d+)', gpword.attrs["id"]).group(1))
 
         return ret
 
     @property
     def floors(self):
-        """ Floor object list of the bahapost """
+        """ Floor object list of the BahaPost """
         ret = []
 
-        for snB in self.floors_snB:
+        for snb in self.floors_snb:
             ret.append(
-                    floor(self.bsn, snB))
+                Floor(self.bsn, snb))
         return ret
 
     @property
     def content(self):
         """ The content of the main floor
-        an alias for html
+        An alias for html
         """
         return self.floors[0].html
 
     @property
     def html(self):
         """ The content of the main floor in HTML """
-        return self.floors[0].getContent(bahaCode=False, prettify=True)
+        return self.floors[0].get_content(baha_code=False, prettify=True)
 
     @property
-    def bahacode(self):
-        """ The content of the main floor in bahacode, from /post1.php
+    def baha_code(self):
+        """ The content of the main floor in baha_code, from /post1.php
         Requies vaild BAHARUNE and BAHAID cookies."""
-        return self.getContent(bahaCode=True, prettify=False)
+        return self.floors[0].get_content(baha_code=True, prettify=False)
 
-class floor:
+class Floor:
     """ An object class used to describe floors of baha posts """
-    def __init__(self, bsn, snB):
+    def __init__(self, bsn, snb):
         """ Initiates a floor object """
         self.bsn = bsn
-        self.snB = snB
+        self.snb = snb
         soup = BeautifulSoup(requests.get("http://forum.gamer.com.tw/Co.php",
-            params = {"bsn": bsn, "sn": snB},
-            headers=REQHEADERS,
-            cookies=REQCOOKIES).text)
+                                          params={"bsn": bsn, "sn": snb},
+                                          headers=REQHEADERS,
+                                          cookies=REQCOOKIES).text)
 
-        for p in soup(id="BH-master")[0]("p", {"class": "FM-lbox1"}):
-            parsed = urlparse.urlparse(p.a.attrs["href"])
+        for p_item in soup(id="BH-master")[0]("p", {"class": "FM-lbox1"}):
+            parsed = urlparse.urlparse(p_item.a.attrs["href"])
             params_ = dict(urlparse.parse_qsl(parsed.query))
             if parsed.path == 'switch.php' and "bsn" in params_:
-                self.bsn = params_["bsn"]
+                self.sna = params_["sna"]
                 break
-
-    def from_url(inurl):
-        """ Initiate a floor object with an URL """
-        parsed = urlparse.urlparse(inurl)
-        params = dict(urlparse.parse_qsl(parsed.query))
-
-        if (parsed.scheme == "http" and
-                parsed.netloc == "forum.gamer.com.tw" and
-                parsed.path == "/Co.php" and
-                all(x in params for x in ["bsn", "sn"])):
-            return floor(params["bsn"], params["sn"])
-        else:
-            raise ValueError("Input is not a vaild bahaurl.")
 
     @property
     def content(self):
+        """ The floor's content in html
+        An alias for html """
         return self.html
 
     @property
     def html(self):
-        """ The floor's content in bahacode, from /Co.php """
-        return self.getContent(bahaCode=False, prettify=True)
+        """ The floor's content in baha_code, from /Co.php """
+        return self.get_content(baha_code=False, prettify=True)
 
     @property
-    def bahacode(self):
-        """ The floor's content in bahacode, from /post1.php
+    def baha_code(self):
+        """ The floor's content in baha_code, from /post1.php
         Requies vaild BAHARUNE and BAHAID cookies. """
-        return self.getContent(bahaCode=True, prettify=False)
+        return self.get_content(baha_code=True, prettify=False)
 
-    def getContent(self, bahaCode=False, prettify=True):
+
+    def get_content(self, baha_code=False, prettify=True):
         """ Retrieve content of a floor
 
-        @param bool bahaCode     Outputs bahaCode from /post1.php when this is set to True, if not, Output HTML from /Co.php .
-        @param bool prettify     Outputs prettified HTML by BeautifulSoup if is set.
+        @param bool baha_code        Outputs baha_code from /post1.php when this is set to True,
+                                    If not, Output HTML from /Co.php .
+        @param bool prettify        Outputs prettified HTML by BeautifulSoup if is set.
         """
 
-        if bahaCode and not prettify:
+        if baha_code and not prettify:
             response = requests.get("http://forum.gamer.com.tw/post1.php",
-                params = {"bsn": self.bsn, "snA": self.snA, "sn": self.snB, "type": "2", "re": "1"},
-                headers=REQHEADERS,
-                cookies=REQCOOKIES)
+                                    params={"bsn": self.bsn,
+                                            "sna": self.sna,
+                                            "sn": self.snb,
+                                            "type": "2", "re": "1"},
+                                    headers=REQHEADERS,
+                                    cookies=REQCOOKIES)
             response.encoding = 'utf8'
             soup = BeautifulSoup(response.text)
-            return re.search("^,bahacode:true,content:'[^']*'",
-                    str(soup(id="form1")[0].find_all("script")),
-                    multiline=True).group(1)
-        elif bahaCode and prettify:
-            raise ValueError('bahaCode and prettify can\'t be true at the same time')
+            return re.search(r"^,baha_code:true,content:'[^']*'",
+                             str(soup(id="form1")[0].find_all("script"),
+                                 multiline=True)).group(1)
+        elif baha_code and prettify:
+            raise ValueError('baha_code and prettify can\'t be true at the same time')
         else:
             response = requests.get("http://forum.gamer.com.tw/Co.php",
-                params = {"bsn": self.bsn, "sn": self.snB},
-                headers=REQHEADERS,
-                cookies=REQCOOKIES)
+                                    params={"bsn": self.bsn, "sn": self.snb},
+                                    headers=REQHEADERS,
+                                    cookies=REQCOOKIES)
             response.encoding = 'utf8'
             soup = BeautifulSoup(response.text)
-            return soup(id="cf"+ self.snB)[0].prettify() if prettify else soup(id="cf"+ self.snB)[0].content
+            text = soup(id="cf" + self.snb)[0]
+            return text.prettify() if prettify else text.text
